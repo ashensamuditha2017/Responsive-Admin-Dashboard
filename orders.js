ea4f72f968b75dc5c3121939b02edf7f97bcdb45
Ashen Samuditha Senarathne <116456922+ashensamuditha2017@users.noreply.github.com>
2024-07-14 10:36:30 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,29 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'LARVENDER XF102 Drone',
+        productNumber: "36378",
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: "Pending"
+    },
+
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
